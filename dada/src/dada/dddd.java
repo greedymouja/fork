0d6aaecf29fa26bb26cd6207248f79c4a22705ca
Admin <Admin@DESKTOP-7O4IF1R>
2023-04-19 12:31:31 +0800

    asd

--- dada/src/dada/dddd.java
+++ dada/src/dada/dddd.java
@@ -4,7 +4,7 @@ public class dddd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aaaaasda");
+		System.out.println("aaaaasdasda");
 	}
 
 }
